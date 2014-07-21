b6c1035b28e8d1b5d2091db5470874f5a3444e82
Eric Lubin <eric@lubin.us>
2014-07-21 14:15:52 -0400

    Fixed list-only error and wrote code to generate DockerFile

--- filesystem.py
+++ filesystem.py
@@ -6,6 +6,7 @@ import subprocess
 import tempfile
 import tarfile
 import shutil
+import json
 
 
 class LinuxInfoParser(object):
@@ -38,6 +39,7 @@ class BaseImageGenerator(object):
         self.path_to_root = os.path.join(os.path.abspath(path_to_root), '') # stupid hack to get the trailing slash
         self.temp_dir = tempfile.mkdtemp()
         self.modified_directory = os.path.join(self.temp_dir, 'modded')
+        self.deleted_list = os.path.join(self.temp_dir, 'deleted.txt')
         os.makedirs(self.modified_directory)
         self.generate_linux_info()
         logging.debug('Detected OS: %s' % self.linux_info['PRETTY_NAME'])
@@ -102,6 +104,40 @@ class BaseImageGenerator(object):
         logging.debug(cmd)
         subprocess.check_output(cmd, shell=True)
 
+    def _generate_deletions_and_modified(self, base_image_root):
+        deleted_dir = os.path.join(self.temp_dir, 'deleted')
+        os.makedirs(deleted_dir)
+        cmd = 'sudo rsync -axHAX --compare-dest=%s %s %s' % (self.path_to_root, base_image_root, deleted_dir)
+        logging.debug(cmd)
+        subprocess.check_output(cmd, shell=True)
+        return self._list_all_files_and_folders(deleted_dir)
+
+    @staticmethod
+    def _list_all_files_and_folders(dir):
+        system_list = list()
+        for dirpath, dirnames, filenames in os.walk(dir):
+            if dirpath != dir:
+                relative_dir = os.path.relpath(dirpath, dir)
+            else:
+                relative_dir = ''
+            system_list.extend([os.path.join(relative_dir, x) for x in filenames])
+            system_list.extend([os.path.join(relative_dir, x) for x in dirnames])
+        # now reverse the order, so that files get deleted before their parents
+        system_list.reverse()
+        return system_list
+
+    def generate_deletions(self, base_image_root):
+        deletions_and_modifications = self._generate_deletions_and_modified(base_image_root)
+        deletions = list()
+        for candidate in deletions_and_modifications:
+            to_check = os.path.join(self.modified_directory, candidate)
+            if not os.path.lexists(to_check):
+                #logging.debug('%s: file does not exist' % to_check)
+                deletions.append(candidate)
+        # TODO: for some reason this isn't working
+        logging.debug('%d modifications and deletions, %d deletions' % (len(deletions_and_modifications), len(deletions)))
+        return deletions
+
     def generate(self):
         repo = self.linux_info['ID']
         tag = self.linux_info['VERSION_ID']
@@ -112,6 +148,44 @@ class BaseImageGenerator(object):
         base_image_root = self.extract_base_image_tar(base_tar_path)
         logging.debug('Generating filesystem diff...')
         self.generate_diff(base_image_root)
+        exclude = {'.dockerinit'}
+        with open(self.deleted_list, 'w') as text_file:
+            # prepend a '/' to every path, we want these to be absolute paths on the new host
+            text_file.write('\n'.join('/' + x for x in self.generate_deletions(base_image_root) if x not in exclude))
+        path = self.create_docker_image(repo, tag)
+        logging.debug('Docker build is now located at: %s' % path)
+
+        # now build it
+        #self.build_and_push_to_registry(path, 'ubuntu_modified')
+        # now push it to the registry (local for now)
+
+    def create_docker_image(self, repo, tag):
+        temp_dir = tempfile.mkdtemp()
+
+        # now tar up the modifications and into the directory
+        changes = 'changes.tar'
+        tar_file = os.path.join(temp_dir, 'changes.tar')
+        subprocess.check_output('sudo tar -C %s -c . -f %s' % (self.modified_directory, tar_file), shell=True)
+
+        # move the deleted
+        deleted = 'deleted.txt'
+        os.rename(self.deleted_list, os.path.join(temp_dir, deleted))
+        df = """FROM %(repo)s:%(tag)s
+ADD %(changes)s /
+ADD %(deleted)s /src/
+RUN xargs -d '\\n' -a /src/%(deleted)s rm -r
+RUN rm -rf /src/%(deleted)s""" % {'repo': repo, 'tag': tag, 'changes': changes, 'deleted': deleted}
+
+        with open(os.path.join(temp_dir, 'Dockerfile'), 'w') as dockerfile:
+            dockerfile.write(df)
+
+        return temp_dir
+
+    def build_and_push_to_registry(self, docker_dir, tag):
+        for x in self.docker_client.build(path=docker_dir, tag=tag):
+            y = json.loads(x)
+            logging.debug(y['stream'])
+
 
     def clean_up(self):
         # delete the temporary directory
